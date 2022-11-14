d4d07c7500bed2c803d6e347db514d7ce29b5f6d
Gabriel Borel <biel_borel@hotmail.com>
2022-11-14 19:11:47 -0300

    validation added

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { ICreateUserDTO } from "./../../dtos/ICreateUserDTO";
 import { inject, injectable } from "tsyringe";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { AppError } from "../../../../errors/AppError";
 
 @injectable()
 export class CreateUserUseCase {
@@ -10,6 +11,12 @@ export class CreateUserUseCase {
   ) {}
 
   async execute({ cellphone, email, name, password }: ICreateUserDTO) {
+    const emailAlreadyInUse = await this.usersRepository.findByEmail(email);
+
+    if (emailAlreadyInUse) {
+      throw new AppError("A user with this email already exists");
+    }
+
     return await this.usersRepository.create({
       cellphone,
       email,
