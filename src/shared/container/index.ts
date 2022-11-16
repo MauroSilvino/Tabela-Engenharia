503e1287da147e1296a512736902dd00c2da457f
Gabriel Borel <biel_borel@hotmail.com>
2022-11-16 10:15:38 -0300

    added itemsRepository to be injected

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,4 +1,6 @@
 import { container } from "tsyringe";
+import { IItemsRepository } from "../../modules/items/repositories/IItemsRepository";
+import { ItemsRepository } from "../../modules/items/repositories/implementations/ItemsRepository";
 import { ProjectsRepository } from "../../modules/projects/repositories/implementations/ProjectsRepository";
 import { IProjectsRepository } from "../../modules/projects/repositories/IProjectsRepository";
 import { UsersRepository } from "../../modules/users/repositories/implementations/UsersRepository";
@@ -12,4 +14,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<IProjectsRepository>(
   "ProjectsRepository",
   ProjectsRepository
+);
+
+container.registerSingleton<IItemsRepository>(
+  "ItemsRepository",
+  ItemsRepository
 );
