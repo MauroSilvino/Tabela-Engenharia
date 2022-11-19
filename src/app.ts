0ed775bd434f521b5ab547b12171a7a7ef621ed0
Gabriel Borel <biel_borel@hotmail.com>
2022-11-19 11:20:47 -0300

    app file created, the entry point

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import "reflect-metadata";
+import "express-async-errors";
+import "./shared/container";
+import express, { NextFunction, Request, Response } from "express";
+import { routes } from "./routes/index";
+import { AppError } from "./errors/AppError";
+import cors from "cors";
+
+export const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: `Internal server error = ${err.message}`,
+  });
+});
