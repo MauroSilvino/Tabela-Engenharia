7ee2ebd2ff76d923f90cb68f064463f75a5abc47
Gabriel Borel <biel_borel@hotmail.com>
2022-12-07 16:57:46 -0300

    made find item by category/description to same route, same controller and usecase

--- src/modules/items/useCases/importItems/ImportItemsUseCase.ts
+++ src/modules/items/useCases/importItems/ImportItemsUseCase.ts
@@ -1,61 +1,66 @@
-import { inject, injectable } from "tsyringe";
-import { ICategoriesRepository } from "../../../categories/repositories/ICategoriesRepository";
-import { IItemsRepository } from "../../repositories/IItemsRepository";
+import {inject, injectable} from "tsyringe";
+import {ICategoriesRepository} from "../../../categories/repositories/ICategoriesRepository";
+import {IItemsRepository} from "../../repositories/IItemsRepository";
 
 interface IData {
-  "codigo 13a edição": string;
-  descrição: string;
-  unid: string;
-  "pr ago/2012": number;
+	"codigo 13a edição": string;
+	descrição: string;
+	unid: string;
+	"pr ago/2012": number;
 }
 
 @injectable()
 export class ImportItemsUseCase {
-  constructor(
-    @inject("ItemsRepository")
-    private itemsRepository: IItemsRepository,
-    @inject("CategoriesRepository")
-    private categoriesRepository: ICategoriesRepository
-  ) {}
-
-  async create(data: IData[]) {
-    let categoryId = 0;
-
-    for (const row of data) {
-      const isCategory =
-        !row["codigo 13a edição"] &&
-        row.descrição &&
-        !row.unid &&
-        !row["pr ago/2012"];
-
-      const isItem =
-        row["codigo 13a edição"] &&
-        row.descrição &&
-        row.unid &&
-        row["pr ago/2012"];
-
-      if (isCategory) {
-        const category = await this.categoriesRepository.create(row.descrição);
-        categoryId = category.id;
-      }
-
-      if (isItem) {
-        const rowId = row["codigo 13a edição"]
-          .replaceAll(".", "")
-          .replaceAll("-", "");
-
-        if (/^\d+$/.test(rowId)) {
-          console.log(categoryId);
-
-          await this.itemsRepository.create({
-            description: row.descrição,
-            id: row["codigo 13a edição"],
-            unity: row.unid,
-            value: row["pr ago/2012"],
-            categoryId,
-          });
-        }
-      }
-    }
-  }
+	constructor(
+		@inject("ItemsRepository")
+		private itemsRepository: IItemsRepository,
+		@inject("CategoriesRepository")
+		private categoriesRepository: ICategoriesRepository
+	) {
+	}
+	
+	async create(data: IData[]) {
+		let categoryId = 0;
+		
+		for (const row of data) {
+			const isCategory =
+				!row["codigo 13a edição"] &&
+				row.descrição &&
+				!row.unid &&
+				!row["pr ago/2012"];
+			
+			const isItem =
+				row["codigo 13a edição"] &&
+				row.descrição &&
+				row.unid &&
+				row["pr ago/2012"];
+			
+			if (isCategory) {
+				const category = await this.categoriesRepository.create(row.descrição);
+				categoryId = category.id;
+			}
+			
+			if (isItem) {
+				const rowId = row["codigo 13a edição"]
+				.replaceAll(".", "")
+				.replaceAll("-", "");
+				
+				const onlyDigit = /^\d+$/;
+				
+				if (onlyDigit.test(rowId)) {
+					try {
+						await this.itemsRepository.create({
+							description: row.descrição,
+							id: row["codigo 13a edição"],
+							unity: row.unid,
+							value: row["pr ago/2012"],
+							categoryId,
+						});
+					} catch (err) {
+						console.log(err);
+					}
+				}
+			}
+		}
+	}
 }
--- src/routes/items.routes.ts
+++ src/routes/items.routes.ts
@@ -1,30 +1,30 @@
-import { CreateItemController } from "./../modules/items/useCases/createItem/CreateItemController";
-import { Router } from "express";
-import { ImportItemsController } from "../modules/items/useCases/importItems/ImportItemsController";
+import {CreateItemController} from "./../modules/items/useCases/createItem/CreateItemController";
+import {Router} from "express";
+import {ImportItemsController} from "../modules/items/useCases/importItems/ImportItemsController";
 import multer from "multer";
-import { FindItemsByDescriptionController } from "../modules/items/useCases/findItemsByDescription/FIndItemsByDescriptionController";
+import {FindItemsController} from "../modules/items/useCases/findItems/FindItemsController";
 
 export const itemsRoutes = Router();
 
 const createItemController = new CreateItemController();
 const importItemsController = new ImportItemsController();
-const findItemsByDescriptionController = new FindItemsByDescriptionController();
+const findItemsController = new FindItemsController();
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
+	destination: function (req, file, cb) {
+		cb(null, "uploads");
+	},
+	filename: function (req, file, cb) {
+		cb(null, Date.now() + "-" + file.originalname);
+	},
 });
 
-const upload = multer({ storage });
+const upload = multer({storage});
 
 itemsRoutes.post("/", createItemController.handle);
-itemsRoutes.get("/", findItemsByDescriptionController.handle);
+itemsRoutes.get("/", findItemsController.handle);
 itemsRoutes.post(
-  "/upload",
-  upload.single("xlsx"),
-  importItemsController.handle
+	"/upload",
+	upload.single("xlsx"),
+	importItemsController.handle
 );
