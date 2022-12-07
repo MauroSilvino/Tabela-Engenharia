332aed707cc703c99d3712023a516d226a24e4b8
Gabriel Borel <biel_borel@hotmail.com>
2022-12-07 10:03:15 -0300

    added create custom_items table when creating a project

--- src/modules/projects/repositories/implementations/ProjectsRepository.ts
+++ src/modules/projects/repositories/implementations/ProjectsRepository.ts
@@ -1,5 +1,5 @@
+import { prismaClient } from "./../../../../database/prismaClient";
 import { Project } from "@prisma/client";
-import { prismaClient } from "../../../../database/prismaClient";
 import { ICreateProjectDTO } from "../../dtos/ICreateProjectDTO";
 import { IProjectsRepository } from "./../IProjectsRepository";
 
@@ -12,6 +12,23 @@ export class ProjectsRepository implements IProjectsRepository {
         authorId,
       },
     });
+
+    await prismaClient.$executeRawUnsafe(
+      `CREATE TABLE custom_items_${authorId}_${project.id} (
+        id INT NOT NULL AUTO_INCREMENT, 
+        description VARCHAR(5000), 
+        unity VARCHAR(50), 
+        quantity VARCHAR(50), 
+        value DOUBLE,
+        user_id INT,
+        project_id INT,
+        category_id INT,
+        FOREIGN KEY(user_id) REFERENCES users(id), 
+        FOREIGN KEY(project_id) REFERENCES projects(id),
+        FOREIGN KEY(category_id) REFERENCES categories(id), 
+        PRIMARY KEY(id)
+      );`
+    );
 
     return project;
   }
