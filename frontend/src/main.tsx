bf9973baf0a2d9ad4b9f2f720c5dbb621fe008bd
Gabriel Borel <biel_borel@hotmail.com>
2022-12-09 19:19:34 -0300

    added routerprovider to main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./pages/App";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+import "./styles/Global.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
