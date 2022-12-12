8f428a97f644d0bf684412565c01cb7d7a9a7d0c
Gabriel Borel <biel_borel@hotmail.com>
2022-12-12 19:34:13 -0300

    register and login routes added

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,9 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
+import { Login } from "./pages/login";
 import { Payment } from "./pages/payment";
+import { Register } from "./pages/register";
 
 export const router = createBrowserRouter([
   {
     path: "/payment",
     element: <Payment />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
