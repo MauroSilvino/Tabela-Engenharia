7cd9bd3c3b743164640b773e2faea20775eff9e2
Gabriel Borel <biel_borel@hotmail.com>
2022-12-12 18:01:01 -0300

    authenticate user route added

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,8 +1,11 @@
 import { CreateUserController } from "./../modules/users/useCases/createUser/CreateUserController";
 import { Router } from "express";
+import { AuthenticateUserController } from "../modules/users/useCases/authenticateUser/AuthenticateUserController";
 
 export const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
+const authenticateUserController = new AuthenticateUserController();
 
 usersRoutes.post("/", createUserController.handle);
+usersRoutes.post("/authenticate", authenticateUserController.handle);
