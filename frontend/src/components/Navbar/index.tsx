33af6fc8df678326d7773fc636983804d1df8bac
PedroaocDev <oliveiracruzpedroaugusto@gmail.com>
2022-12-14 16:43:45 -0300

    adding react-icons

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,15 +1,23 @@
 import styles from "./Navbar.module.scss";
 import profileSvg from "../../assets/profile.svg";
 import menuSvg from "../../assets/menu.svg";
+import { CgProfile } from "react-icons/cg"
+import { FiMenu } from "react-icons/fi"
+
 
 export const Navbar = () => {
   return (
-    <nav className={styles.container}>
+    <nav className={styles.container}> 
       <div className={styles.menu}>
-        <img src={menuSvg} alt="ícone de menu" />
+        <FiMenu
+          color="#EDEDF5"
+          size={40}/>
+       
       </div>
       <div className={styles.profile}>
-        <img src={profileSvg} alt="ícone de perfil" />
+        <CgProfile 
+          color="#EDEDF5"
+          size={40}/>
       </div>
     </nav>
   );
