d5490e9eacea8dc887e703361a0d980aeb320b0c
PedroaocDev <oliveiracruzpedroaugusto@gmail.com>
2022-12-15 20:33:07 -0300

    new routes added

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,8 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "./components/Layout";
+import { ForgotPassword } from "./pages/ForgotPassword";
 import { Login } from "./pages/Login";
 import { Payment } from "./pages/Payment";
+import { RecoverPassword } from "./pages/RecoverPassword";
 import { Register } from "./pages/Register";
+import { ResetPassword } from "./pages/ResetPassword";
 
 export const router = createBrowserRouter([
   {
@@ -20,6 +23,18 @@ export const router = createBrowserRouter([
       {
         path: "/login",
         element: <Login />,
+      },
+      {
+        path: "/forgotpassword",
+        element: <ForgotPassword />,
+      },
+      {
+        path: "/recoverpassword",
+        element: <RecoverPassword/>,
+      },
+      {
+        path: "/resetpassword",
+        element: <ResetPassword/>,
       },
     ],
   },
