3b3eb6b4497adb8588d8e5801f027891b868ec61
PedroaocDev <oliveiracruzpedroaugusto@gmail.com>
2022-12-22 09:24:06 -0300

    validations were added to the Message component

--- frontend/src/pages/Home/components/Message/index.tsx
+++ frontend/src/pages/Home/components/Message/index.tsx
@@ -1,22 +1,71 @@
 import styles from "./Message.module.scss";
+import * as yup from "yup";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+
+
+interface MessageFormSchema {
+    name: string;
+    email: string;
+    message: string;
+  }
+  
+  const messageFormSchema = yup
+    .object()
+    .shape({
+      name: yup.string().required("Nome completo é obrigatório."),
+      email: yup
+        .string()
+        .email("Email inválido")
+        .required("Email é obrigatório."),
+      message: yup.string().required("Mensagem é obrigatória.")
+    })
+    .required();
+  
+  
 
 export const Message = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm<MessageFormSchema>({
+        resolver: yupResolver(messageFormSchema),
+      });
+    
+      const handleFormSubmit: SubmitHandler<FieldValues> = (data) => {
+        
+    
+        console.log(data);
+      };
+
     return (
         <section className={styles.container}>
             <h2>Ficou na {""} <strong>dúvida?</strong>Envie uma {""} <strong>mensagem</strong></h2>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={handleSubmit(handleFormSubmit)}>
+
                 <div className={styles.inputContainer}>
                     <label htmlFor="fullName">Nome completo</label>
-                    <input type="text" id="fullName"/>
+                    {errors.name && (
+                        <span className={styles.inputError}>{errors.name.message}</span>
+                    )}
+                    <input type="text" id="fullName" {...register("name")}/>
                 </div>
                 <div className={styles.inputContainer}>
                     <label htmlFor="email">E-mail</label>
-                    <input type="email" id="email" />
+                    {errors.email && (
+                        <span className={styles.inputError}>{errors.email.message}</span>
+                    )}
+                    <input type="email" id="email" {...register("email")}/>
                 </div>
 
                 <div className={styles.inputContainer}>
                     <label htmlFor="message">Mensagem</label>
-                    <textarea id="message"  ></textarea>
+                    {errors.message && (
+                        <span className={styles.inputError}>{errors.message.message}</span>
+                    )}
+                    <textarea id="message" {...register("message")} ></textarea>
                 </div>
                 
                 <button type="submit" className={styles.submitBtn}>
