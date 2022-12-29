b737885b1b3da7721d59c4dea108ab845bd13c4d
Gabriel Borel <biel_borel@hotmail.com>
2022-12-29 15:53:45 -0300

    reset password route added

--- backend/src/routes/password.routes.ts
+++ backend/src/routes/password.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import { ResetPasswordController } from "../modules/users/useCases/resetPassword/ResetPasswordController";
 import { SendPasswordRecoveryMailController } from "../modules/users/useCases/sendPasswordRecoveryMail/SendPasswordRecoveryMailController";
 
 export const passwordRoutes = Router();
 
 const sendPasswordRecoveryMailController =
   new SendPasswordRecoveryMailController();
+const resetPasswordController = new ResetPasswordController();
 
 passwordRoutes.post("/recovery", sendPasswordRecoveryMailController.handle);
+passwordRoutes.post("/reset", resetPasswordController.handle);
