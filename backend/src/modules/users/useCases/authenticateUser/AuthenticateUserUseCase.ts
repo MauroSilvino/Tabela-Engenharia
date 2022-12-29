5b5b8a65b6e39e63c724b1366a4fea2134c33672
Gabriel Borel <biel_borel@hotmail.com>
2022-12-29 15:21:09 -0300

    refactor adding usersToeksn repository

--- backend/src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ backend/src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,20 +2,26 @@ import { compare } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 import { AppError } from "../../../../errors/AppError";
+import { IDateProvider } from "../../../../shared/container/providers/DateProvider/IDateProvider";
 import { IAuthenticateUserDTO } from "../../dtos/IAuthenticateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { IUsersTokensRepository } from "../../tokens/repositories/IUsersTokensRepository";
 
 @injectable()
 export class AuthenticateUserUseCase {
   constructor(
     @inject("UsersRepository")
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+    @inject("UsersTokensRepository")
+    private usersTokensRepository: IUsersTokensRepository,
+    @inject("DateProvider")
+    private dateProvider: IDateProvider
   ) {}
 
   async execute({ email, password }: IAuthenticateUserDTO) {
     const user = await this.usersRepository.findByEmail(email);
 
-    if (!!!user) throw new AppError("Email or password incorrect.");
+    if (!user) throw new AppError("Email or password incorrect.");
 
     const passwordMatch = await compare(password, user.password);
 
@@ -30,6 +36,12 @@ export class AuthenticateUserUseCase {
         expiresIn: "1h",
       }
     );
+
+    await this.usersTokensRepository.create({
+      token,
+      userId: user.id,
+      expiresDate: this.dateProvider.addDays(7),
+    });
 
     return { token };
   }
